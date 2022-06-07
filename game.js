d9f9794b4b3359dd96e36b8635264009850e855d
Raymond Mojica <rm@M12020.home>
2022-06-07 17:53:01 -0400

    Game Board created

--- game.js
+++ game.js
@@ -1,7 +1,7 @@
 //Declare Variables
 const grid = document.querySelector('.Grid')
 
-for (let i=0; i < 225; i++){
+for (let i=0; i < 1750; i++){
     const square = document.createElement('div')
     grid.appendChild(square)
 }
